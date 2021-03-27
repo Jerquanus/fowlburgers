321e054aa5da3208530d5cffacb0b4e80de239ce
Jerquanus Swann <jerquanus@gmail.com>
2021-03-27 15:14:05 -0400

    DB server created, config folder created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Fowl Finger Combo', false);
+INSERT INTO burgers (name, devoured) VALUES ('Fowlburger Fertilized Style', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Original Fowlburger', true);
